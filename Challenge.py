23318e80338490123345f5ec5b3a22f436080aa4
TarasMaru <to.marusov@gmail.com>
2020-05-11 12:12:00 +0300

    Update Challenge.py
     code shortening

--- Challenge.py
+++ Challenge.py
@@ -7,13 +7,12 @@ def my_counter(file_name):
         file_data = open(file_name, mode='r', encoding='UTF-8')
         content = file_data.read()
         file_data.close()
-    except:
-        print(f'File cannot be opened or read: {file_name}')
+    except Exception as Ex:
+        print(f'Error message >>> {Ex}')
         quit()
     try:
-        pattern = compile(r'\b[a-zA-Z]+')  # r'\b[a-zA-Z]+-?[a-zA-Z]*' - to capture hyphenated words
+        pattern = compile(r'\b[a-zA-Z]+')
         matches = pattern.findall(content)
-        # Count words occurrence
         words_count = dict()
         for word in sorted(matches):
             words_count[word.upper().lower().capitalize()] = words_count.get(word.upper().lower().capitalize(), 0) + 1
@@ -25,10 +24,5 @@ def my_counter(file_name):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('-p', '--path', type=str, help='enter absolute file path', required=True)
-    args = parser.parse_args()
-    file_path = args.path
-    result = my_counter(file_name=file_path)
-    print(result)
-
-
-
+    file_path = parser.parse_args().path
+    print(my_counter(file_name=file_path))
